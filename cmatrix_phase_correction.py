e66c0d63c56efb0028a2fa29d6100f8564a4001d
orbarton <oliver.r.barton@gmail.com>
2024-08-20 02:31:58 +0100

    Update cmatrix_phase_correction.py

--- cmatrix_phase_correction.py
+++ cmatrix_phase_correction.py
@@ -11,6 +11,7 @@ from google.colab import files
 uploaded = files.upload()
 
 import numpy as np
+#Here matrixdimension should be the square root of the matrix total size, or the size of either the x or y dimension
 def phase_correct(file, matrixdimension):
   #Load the file pre-correction
   correction = np.load(file)
@@ -18,7 +19,7 @@ def phase_correct(file, matrixdimension):
   reference = correction[0]
   #Phase correct
   for i in range(1,1000):
-    for j in range(105):
+    for j in range(matrixdimension):
       matrix = correction[i][j]
       if np.dot(reference[j], matrix) < 0:
         correction[i][j] = correction[i][j]*-1
