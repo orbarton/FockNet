dcd31384fe0bb9aee0d0698cd0d7556ac8be39cc
orbarton <oliver.r.barton@gmail.com>
2024-08-27 10:05:40 +0100

    Update modeltraining.py

--- Model/modeltraining.py
+++ Model/modeltraining.py
@@ -10,6 +10,7 @@ Original file is located at
 def training(custom_data_one, modelname, version, fockmatrixtutone):
   import pytorch_lightning as pl
   import schnetpack as spk
+  import os
   logger = pl.loggers.TensorBoardLogger(save_dir=fockmatrixtutone,name=f'{version}')
   callbacks = [
     spk.train.ModelCheckpoint(
